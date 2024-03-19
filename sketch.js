023c751efab1254581eed07c6d2fcc1f0fbf89ff
Sergey Belyaev <79952005+sbelka-1703@users.noreply.github.com>
2024-03-18 17:53:09 -0700

    now its bouncing of the bottom and top

--- sketch.js
+++ sketch.js
@@ -1,21 +1,20 @@
 class Ellipse {
-  constructor(x, y, size) {
+  constructor(x, y, size, velocity) {
     this.x = x;
     this.y = y;
     this.size = size;
+    this.velocity = velocity;
   }
   display() {
-    ellipse(this.x, this.y, this.size, this.size);
+    ellipse(this.x, this.y, this.size, this.size, this.velocity);
   }
 
   move() {
-    if (this.y < height) {
-      this.y += 1; // Move the ellipse down each frame
-      console.log("1");
-    } else if (this.y >= height) {
-      // Check if the ellipse has moved beyond the canvas
-      this.y -= 1;
-      console.log("2");
+    this.y += this.velocity;
+    this.x += this.velocity;
+
+    if (this.y <= 0 || this.y + this.size >= height) {
+      this.velocity *= -1;
     }
   }
 }
@@ -24,12 +23,13 @@ let myEllipse;
 
 function setup() {
   createCanvas(400, 400);
-  myEllipse = new Ellipse(50, 50, 5);
-  myEllipse2 = new Ellipse(100, 100, 10);
+  myEllipse = new Ellipse(50, 50, 5, 1);
+  myEllipse2 = new Ellipse(100, 50, 10, 1.2);
 }
 function draw() {
   background(0);
   myEllipse.display();
   myEllipse.move();
   myEllipse2.display();
+  myEllipse2.move();
 }
